9e448bcbe61d35ba7a95b8bba634ad7399ea7e5c
Lisa Kosiachenko <ekosyachenko@gmail.com>
2016-12-25 22:19:28 -0800

    fixed formatting in InvIndexes.java

--- InvIndexes.java
+++ InvIndexes.java
@@ -50,51 +50,46 @@ public class InvIndexes {
     public static class Reduce extends MapReduceBase implements Reducer<Text, Text, Text, Text> {
     
 		public void reduce(Text key, Iterator<Text> values, OutputCollector<Text, Text> output, Reporter reporter) throws IOException {
-		
-		    Text docListText = new Text();
-            String docListString = "";
+			Text docListText = new Text();
+			String docListString = "";
 			
 			HashMap<String, Integer> hashmap = new HashMap<String, Integer>();
 			
 			while (values.hasNext()) {
-			    Text value = values.next();
-			    String line = value.toString();
-			    
-		        StringTokenizer tokenizer = new StringTokenizer(line);
-		        
-		        while (tokenizer.hasMoreTokens()) {
-				    String filename = new String(tokenizer.nextToken());
-				    if (tokenizer.hasMoreTokens()) {
-				        int count = Integer.parseInt(tokenizer.nextToken());
-				        int curVal = hashmap.get(filename)!= null ? hashmap.get(filename) : 0;
-				        hashmap.put(filename, curVal + count);
-				    }
+				Text value = values.next();
+				String line = value.toString();
+				StringTokenizer tokenizer = new StringTokenizer(line);
+				while (tokenizer.hasMoreTokens()) {
+					String filename = new String(tokenizer.nextToken());
+					if (tokenizer.hasMoreTokens()) {
+						int count = Integer.parseInt(tokenizer.nextToken());
+						int curVal = hashmap.get(filename)!= null ? hashmap.get(filename) : 0;
+						hashmap.put(filename, curVal + count);
+					}
 				}
-           	}
+			}
+			
+			//Sorting documents by number of occurences of keyword:
            	
-           	//Sorting documents by number of occurences of keyword:
-           	
-            List<HashMap.Entry<String, Integer>> entries = new LinkedList<HashMap.Entry<String, Integer>>(hashmap.entrySet());
-            Collections.sort(entries, new Comparator<HashMap.Entry<String, Integer>> ()
-            {
-                public int compare(HashMap.Entry<String, Integer> entry1, HashMap.Entry<String, Integer> entry2) {
-                    return (entry2.getValue().compareTo(entry1.getValue()));
-                
-                }
-            });
-            
-            for (HashMap.Entry<String, Integer> entry : entries) {
+			List<HashMap.Entry<String, Integer>> entries = new LinkedList<HashMap.Entry<String, Integer>>(hashmap.entrySet());
+			Collections.sort(entries, new Comparator<HashMap.Entry<String, Integer>> ()
+			{
+				public int compare(HashMap.Entry<String, Integer> entry1, HashMap.Entry<String, Integer> entry2) {
+					return (entry2.getValue().compareTo(entry1.getValue()));
+				}
+			});
 
+			for (HashMap.Entry<String, Integer> entry : entries) {
 				docListString += entry.getKey();
 				docListString += " ";
 				docListString += entry.getValue();
 				docListString += " ";
 			}
-	        
-	        docListText.set(docListString);
+
+			docListText.set(docListString);
 			output.collect(key, docListText);  //where docListText is a string concatenation of all filename_counts
+			}
 		}
-	}
 
     public static void main(String[] args) throws Exception {
 		// input format:
