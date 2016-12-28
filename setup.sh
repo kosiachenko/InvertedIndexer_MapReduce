daf76eb47a426e53d9e436f05cb51b83eb5a82b9
Elizaveta Kosiachenko <ekosyachenko@gmail.com>
2016-12-27 23:56:01 -0800

    removed setup.sh since the project is ported to Maven

--- setup.sh
+++
@@ -1,6 +0,0 @@
-../bin/hdfs dfs -rm /user/elizavet/output/*
-../bin/hdfs dfs -rmdir /user/elizavet/output
-javac -cp `../bin/hadoop classpath`:. InvIndexes.java
-jar -cvf InvIndexes.jar *.class
-../bin/hadoop jar InvIndexes.jar InvIndexes rfc output TCP UDP LAN PPP HDLC
-../bin/hdfs dfs -cat /user/elizavet/output/part-00000
